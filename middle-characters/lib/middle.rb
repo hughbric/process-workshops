fe280418439ddbe2446d4a51176784258e9d8018
Michael Hughes <mhughes36@live.co.uk>
2019-06-27 09:41:11 +0100

    returns middle letter of odd word

--- middle-characters/lib/middle.rb
+++ middle-characters/lib/middle.rb
@@ -1,3 +1,9 @@
 class Middle
+  def getMiddle(word)
+    return '' if word.empty?
 
+    characters = word.chars
+    mid = characters.length / 2
+    return characters[mid]
+  end
 end
--- middle-characters/spec/middle_spec.rb
+++ middle-characters/spec/middle_spec.rb
@@ -1,4 +1,13 @@
 require 'middle'
 
 describe Middle do
+  it 'returns an empty string' do
+    middle = Middle.new
+    expect(middle.getMiddle('')).to eq('')
+  end
+
+  it 'returns the middle letter' do
+    middle = Middle.new
+    expect(middle.getMiddle('tests')).to eq('s')
+  end
 end
