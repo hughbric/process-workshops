d8c872ef684ff0a9048b4f87cbbd37615f034847
Michael Hughes <mhughes36@live.co.uk>
2019-07-04 09:18:31 +0100

    acceptance criteria met

--- checkout/lib/checkout.rb
+++ checkout/lib/checkout.rb
@@ -1,2 +1,52 @@
-class Checkout
+# Shop class to calculate total for checkout goods
+class Shop
+  def initialize
+    @total = 0
+    @offer_a = 0
+    @offer_b = 0
+  end
+
+  def checkout(goods)
+    return -1 unless goods.to_s.scan(/[^A-Z]/).empty?
+
+    goods.chars.each do |item|
+      @total += PRICE[item]
+      special_offer(item)
+    end
+    @total
+  end
+
+  private
+
+  PRICE = {
+    'A' => 50,
+    'B' => 30,
+    'C' => 20,
+    'D' => 15
+  }
+
+  def special_offer(item)
+    case item
+    when 'A'
+      offer_a
+    when 'B'
+      offer_b
+    end
+  end
+
+  def offer_a
+    @offer_a += 1
+    if @offer_a == 3
+      @total -= 20
+      @offer_a = 0
+    end
+  end
+
+  def offer_b
+    @offer_b += 1
+    if @offer_b == 2
+      @total -= 15
+      @offer_b = 0
+    end
+  end
 end
--- checkout/spec/checkout_spec.rb
+++ checkout/spec/checkout_spec.rb
@@ -1,4 +1,38 @@
 require 'checkout'
 
-describe Checkout do
+describe Shop do
+  it 'returns -1 if not letter not capital' do
+    shop = Shop.new
+    expect(shop.checkout('aBc')).to eq(-1)
+  end
+
+  it 'returns -1 if not letter not capital' do
+    shop = Shop.new
+    expect(shop.checkout('-B8x')).to eq(-1)
+  end
+
+  it 'returns -1 if not letter not capital' do
+    shop = Shop.new
+    expect(shop.checkout(18)).to eq(-1)
+  end
+
+  it 'returns -1 if not letter not capital' do
+    shop = Shop.new
+    expect(shop.checkout('AA')).to eq(100)
+  end
+
+  it 'returns -1 if not letter not capital' do
+    shop = Shop.new
+    expect(shop.checkout('ABCD')).to eq(115)
+  end
+
+  it 'returns -1 if not letter not capital' do
+    shop = Shop.new
+    expect(shop.checkout('AAA')).to eq(130)
+  end
+
+  it 'returns -1 if not letter not capital' do
+    shop = Shop.new
+    expect(shop.checkout('AAAAAA')).to eq(260)
+  end
 end
