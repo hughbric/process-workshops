b4bc3c69df486114338b7532507412f409151fc2
Michael Hughes <mhughes36@live.co.uk>
2019-07-05 15:40:22 +0100

    raises two argument errors

--- sudoku/lib/sudoku.rb
+++ sudoku/lib/sudoku.rb
@@ -1,3 +1,8 @@
 class Sudoku
-
+  def check(sudoku)
+    raise ArgumentError, "Invalid length" if sudoku.chars.length != 81
+    raise ArgumentError, "Incomplete" if sudoku.chars.include?('0')
+    
+    true
+  end
 end
--- sudoku/spec/sudoku_spec.rb
+++ sudoku/spec/sudoku_spec.rb
@@ -1,5 +1,27 @@
 require 'sudoku'
 
 describe Sudoku do
+  it 'returns true when rows are correct length' do
+    sudoku = Sudoku.new
+    input = "123456789123456789123456789123456789123456789123456789123456789123456789123456789"
+    expect(sudoku.check(input)).to eq(true)
+  end
 
+  it 'raises an argument error if input string is too long' do
+    sudoku = Sudoku.new
+    input = "111123456789123456789123456789123456789123456789123456789123456789123456789123456789"
+    expect { sudoku.check(input) }.to raise_error(ArgumentError, "Invalid length")
+  end
+
+  it 'raises an argument error if it is incomplete (contains 0)' do
+    sudoku = Sudoku.new
+    input = "023456789123456789123456789123456789123456789123456789123456789123456789123456789"
+    expect { sudoku.check(input) }.to raise_error(ArgumentError, "Incomplete")
+  end
+
+  xit 'returns false when each row has repeat numbers 1-9' do
+    sudoku = Sudoku.new
+    input = "113456789123456789123456789123456789123456789123456789123456789123456789123456789"
+    expect(sudoku.check(input)).to eq(false)
+  end
 end
