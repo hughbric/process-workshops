d4fe220eea12d8a437b86625b3527bec31b17b84
Michael Hughes <mhughes36@live.co.uk>
2019-07-25 13:29:03 +0100

    counts colours in the report

--- review/lib/report.rb
+++ review/lib/report.rb
@@ -2,6 +2,10 @@ class Report
   def calculate(report)
     raise ArgumentError, 'Input is empty' if report == ""
     
+    output = ''
+    report_words = report.split(',')
+
+    output += "green: #{report_words.count('green')}, amber: #{report_words.count('amber')}, red: #{report_words.count('red')}"
   end
 
 end
--- review/spec/report_spec.rb
+++ review/spec/report_spec.rb
@@ -4,7 +4,12 @@ describe Report do
   it 'throws an error if input is empty' do
     report = Report.new
     expect { report.calculate("") }.to raise_error(ArgumentError, "Input is empty")
-
   end
+
+  it 'counters the number of colours in the report' do
+    report = Report.new
+    expect(report.calculate("green,amber,red")).to eq("green: 1, amber: 1, red: 1")
+  end
+
 
 end
